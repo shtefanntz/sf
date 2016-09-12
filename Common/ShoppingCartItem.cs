eb8370ce63822fa9ee3a55050f2429d6efd43dec
StefanT <shtefanntz@yahoo.com>
2016-09-12 07:09:49 +0300

    first

---
+++ Common/ShoppingCartItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ShoppingCartItem
+    {
+      public string ProductName { get; set; }
+      public double  UnitPrice { get; set; }
+      public int Amount { get; set; }
+      public double LineTotal => Amount * UnitPrice;
+    }
+
+  [ServiceContract]
+  public interface IShoppingCartService
+  {
+    [OperationContract]
+    Task AddItem(ShoppingCartItem item);
+
+    [OperationContract]
+    Task DeleteItem(string productName);
+
+    [OperationContract]
+    Task<List<ShoppingCartItem>> GetItems();
+  }
+}
